e055df919c6c0b18f18356cb0b5f54eca8527cde
delso-ferreira <delsofelipe@gmail.com>
2023-03-04 15:44:38 -0300

    requisito vqv

--- src/vqv.js
+++ src/vqv.js
@@ -18,6 +18,25 @@
   Caso a função seja chamada sem nenhum parâmetro, o valor undefined deve ser retornado.
 */
 /* ? */
-const vqv = () => {};
+/* const mensagem = 'trabalho na Trybe e mando muito em programação!';
+const foguete = '#VQV!';
+trybe: mensagem,
+hashtag: foguete,
+ */  
+
+const vqv = (nome, numero) => {
+  const estudante = {
+    pessoa: nome,
+    idade: numero,    
+  };
+
+  if (!nome || !numero) {
+    return undefined;
+  } 
+      return `Oi, meu nome é ${estudante.pessoa}!
+Tenho ${estudante.idade} anos,
+trabalho na Trybe e mando muito em programação!
+#VQV!`;
+};
 
 module.exports = vqv;
